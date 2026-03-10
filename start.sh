236f96807165ee3635e0ef942dd2f27772b7c8ff
Jung Lee <13139629+lihao0811@users.noreply.github.com>
2026-03-10 13:35:42 +0800

    Configure PostgreSQL storage

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+export PGSTORE_DSN="postgresql://postgres:kzknCJHVzfYZZNYskUJhvyJkOdyjeSTI@postgres-cjmj.railway.internal:5432/railway"
+export PGSTORE_SCHEMA="public"
+export PGSTORE_LOCAL_PATH="/data/pgstore"
+
 if [ ! -f /CLIProxyAPI/config.yaml ]; then
     cp /CLIProxyAPI/railway.yaml /CLIProxyAPI/config.yaml
 fi
